b1f0485e6df2a3fa26a125cd4e1ee7c4e6de987f
karunsane <karunsane@gmail.com>
2020-08-02 21:24:40 -0500

    Custom endpoint

--- src/main/java/com/fca/dev/springboot/app/users/models/dao/UserDao.java
+++ src/main/java/com/fca/dev/springboot/app/users/models/dao/UserDao.java
@@ -1,13 +1,18 @@
 package com.fca.dev.springboot.app.users.models.dao;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.fca.dev.springboot.app.users.models.entity.Users;
 
 @RepositoryRestResource(path="users")
 public interface UserDao extends PagingAndSortingRepository<Users, Long>{
 	
-	public Users findByUsername(String username);
+	@RestResource(path = "find-username")
+	public Users findByUsername(@Param("username") String username);
+	
+	
 	
 }
