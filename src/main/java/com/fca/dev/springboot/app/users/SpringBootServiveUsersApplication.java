6604c32002a3cac9ddcb42d180089922164f50b0
karunsane <karunsane@gmail.com>
2020-08-05 12:56:57 -0500

    Entity Scan

--- src/main/java/com/fca/dev/springboot/app/users/SpringBootServiveUsersApplication.java
+++ src/main/java/com/fca/dev/springboot/app/users/SpringBootServiveUsersApplication.java
@@ -2,8 +2,10 @@ package com.fca.dev.springboot.app.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EntityScan({"com.fca.dev.springboot.app.users.commons.models.entity"})
 @SpringBootApplication
 public class SpringBootServiveUsersApplication {
 
