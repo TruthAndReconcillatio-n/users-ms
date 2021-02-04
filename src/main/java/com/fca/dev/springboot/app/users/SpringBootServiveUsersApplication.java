e48f1ccfb429c336887bc02915463106bfb4b756
karunsane <karunsane@gmail.com>
2021-02-03 21:03:22 -0600

    Update

--- src/main/java/com/fca/dev/springboot/app/users/SpringBootServiveUsersApplication.java
+++ src/main/java/com/fca/dev/springboot/app/users/SpringBootServiveUsersApplication.java
@@ -3,7 +3,6 @@ package com.fca.dev.springboot.app.users;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EntityScan({"com.fca.dev.springboot.app.users.commons.models.entity"})
 @SpringBootApplication
--- src/main/java/com/fca/dev/springboot/app/users/models/dao/UserDao.java
+++ src/main/java/com/fca/dev/springboot/app/users/models/dao/UserDao.java
@@ -4,7 +4,7 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
-import com.fca.dev.springboot.app.users.commons.models.entity.Users;
+import com.fca.dev.springboot.app.users.models.entity.Users;
 
 @RepositoryRestResource(path = "users")
 public interface UserDao extends PagingAndSortingRepository<Users, Long> {
